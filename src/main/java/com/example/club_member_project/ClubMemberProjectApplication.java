b6682d83431babe6e3a5d063400ca0fd4774d54d
hsun0987 <hsun0987@daum.net>
2025-07-28 15:48:44 +0900

    project init

---
+++ src/main/java/com/example/club_member_project/ClubMemberProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.club_member_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMemberProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubMemberProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/club_member_project/ClubMemberProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.club_member_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubMemberProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
